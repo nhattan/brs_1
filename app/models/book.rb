024ec8ff75b90b54c5c0298da5ad811cc0942e5c
Nhat Tan <nhattan2012@gmail.com>
2014-03-31 09:13:40 +0700

    crud books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ActiveRecord::Base
+  validates :category_id, presence: true
+  validates :title, presence: true
+  validates :summary, presence: true
+  validates :page, presence: true, numericality: {greater_than: 0}
+  validates :author, presence: true
+  validates :published_at, presence: true
+
+  belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Brs1::Application.routes.draw do
   namespace :admin do
     resources :categories
     resources :users
+    resources :books
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
